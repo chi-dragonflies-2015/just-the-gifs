8aac1f393e0a73877d31feca248525f779aec3cd
Matt <apprentice@dbc29.local>
2015-06-25 15:37:48 -0500

    tumblr gem

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+require 'tumblr_client'
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
