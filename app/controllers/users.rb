3bc3b310dc7835126ea1e2c7eaa8293d738677a7
Matt <apprentice@dbc29.local>
2015-06-25 17:47:06 -0500

    user routes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -20,3 +20,30 @@ get '/users/:id' do |id|
   @current_user = current_user
   erb :"/users/show"
 end
+
+get '/users' do
+  @users = User.all
+  erb :"/users/index"
+end
+
+get '/users/:id/edit' do |id|
+  @user = User.find_by(id: id)
+  redirect "/auctions/#{id}" unless authorized?(user)
+  erb :"users/edit"
+
+put '/users/:id' do |id|
+  @user = User.find_by(id: id)
+  redirect "/auctions/#{id}" unless authorized?(user)
+  if @user.update_attributes(params)
+    redirect "/users/#{id}"
+  else
+    erb :"/users/edit"
+  end
+end
+
+delete '/users/:id' do |id|
+  @user = User.find_by(id: id)
+  redirect "/auctions/#{id}" unless authorized?(user)
+  @user.destroy
+  redirect '/users'
+end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -7,7 +7,7 @@ helpers do
     User.find_by(id: session[:user_id])
   end
 
-  def authorized?(user_id)
-    user_id == current_user.id
+  def authorized?(user)
+    user == current_user
   end
 end
