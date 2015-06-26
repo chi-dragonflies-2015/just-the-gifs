2acff2a72193c9ad3b1d3819c2d6582f0187175d
Matt <apprentice@dbc29.local>
2015-06-26 10:56:12 -0500

    post parameters

--- app/controllers/gif_search.rb
+++ app/controllers/gif_search.rb
@@ -4,7 +4,15 @@ get '/gif_search' do
   responses = $tumblr_client.tagged(@search_term, {offset: @offset})
   #response only handles photo posts, not photosets
   gif_responses = responses.select{|post| post.type=="photo" && post.tags.include?("gif")}
-
-
+  @posts = []
+  gif_responses.each do |response|
+    post = {}
+    post[:post_url] = response["post_url"]
+    post[:category] = @search_term
+    post[:image_url] = response["photos"].first["original_size"]["url"]
+    post[:image_width] = response["photos"].first["original_size"]["width"]
+    post[:image_height] = response["photos"].first["original_size"]["height"]
+    @posts.push(post)
+  end
   erb :results
 end
